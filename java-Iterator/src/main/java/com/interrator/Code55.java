82eed80a82d901d80033c7fdc0145d4b8652152d
dengjiayu <965817262@qq.com>
2019-10-08 10:22:32 +0800

    在两个有序数组中找到中位数

---
+++ java-Iterator/src/main/java/com/interrator/Code55.java
@@ -0,0 +1,189 @@
+package com.interrator;
+
+public class Code55 {
+    public static void main(String[] ags) {
+        int[] arr = new int[]{5,1,2};
+        pathsToDitans(arr);
+        for (int i:arr
+             ) {
+            System.out.println(i);
+        }
+        //pathsToDitans(arr);
+        System.out.println(arr);
+        //arr = new int[]{3,2,5};
+        //System.out.println(getCountSum(arr));
+        int[] s1 = new int[]{1,3};
+        int[] s2 = new int[]{2};
+        System.out.println(findMedianSortedArrays1(s1,s2));
+    }
+
+    /**
+     * 在两个有序数组中找到中位数
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // 为了让搜索范围更小，我们始终让 num1 是那个更短的数组，PPT 第 9 张
+        // 上述交换保证了 m <= n，在更短的区间 [0, m] 中搜索，会更快一些
+        int m = nums1.length;
+        int n = nums2.length;
+
+        // 使用二分查找算法在数组 nums1 中搜索一个索引 i，PPT 第 9 张
+        int left = 0;
+        int right = m;
+
+        // 因为 left_total 这个变量会一直用到，因此单独赋值，表示左边粉红色部分一共需要的元素个数
+        int totalLeft = (m + n + 1) >>> 1;
+        while (left < right) {
+            int i = (left + right) >>> 1;
+            int j = totalLeft - i;
+            if (nums2[j - 1] > nums1[i]) {
+                left = i + 1;
+            } else {
+                right = i;
+            }
+        }
+
+        // 退出循环的时候，交叉小于等于一定关系成立，那么中位数就可以从"边界线"两边的数得到，原因在 PPT 第 2 张、第 3 张
+
+        int i = left;
+        int j = totalLeft - left;
+        // 边界值的特殊取法的原因在 PPT 第 10 张
+        int nums1LeftMax = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
+        int nums1RightMin = i == m ? Integer.MAX_VALUE : nums1[i];
+
+        int nums2LeftMax = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
+        int nums2RightMin = j == n ? Integer.MAX_VALUE : nums2[j];
+
+        // 交叉小于等于关系成立，那么中位数就可以从"边界线"两边的数得到，原因在 PPT 第 2 张、第 3 张
+        // 已经找到解了，分数组之和是奇数还是偶数得到不同的结果，原因在 PPT 第 2 张
+        if (((m + n) & 1) == 1) {
+            return Math.max(nums1LeftMax, nums2LeftMax);
+        } else {
+            return (double) ((Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin))) / 2;
+        }
+    }
+
+
+    public static double findMedianSortedArrays1(int[] s1,int[] s2){
+        if (s1.length > s2.length) {
+            int[] temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+        int left = 0;
+        int right = s1.length-1;
+        int m = 0;
+        int k = (s1.length+s2.length+1)/2;
+        while(left<=right){
+            m = (left+right)/2;
+            int j = k-m;
+            if(s1[m]<s2[j-1]){
+                left=m+1;
+            }else {
+                right = m;
+            }
+        }
+        int i = left;
+        int j = k-i;
+        int lM1 = i==0?Integer.MIN_VALUE:s1[i-1];
+        int lM2 = j==0?Integer.MIN_VALUE:s2[j-1];
+        int rM1 = i==s1.length?Integer.MAX_VALUE:s1[i];
+        int rM2 = j==s2.length?Integer.MAX_VALUE:s2[j];
+        if(((s1.length+s2.length)&1)==1){
+            return Math.max(lM1,lM2);
+        }else {
+            return (double)(Math.max(lM1,lM2)+Math.min(rM1,rM2))/2;
+        }
+    }
+
+
+    /**
+     * 给定一个数组,数组下标为城市号,值为到首都路径上第一个相邻的城市,数组值和数组下标相同时为首都,
+     * 城市与首都的距离,每种各有多少个
+     * 时间复杂度O(N)
+     * 空间复杂度1
+     * @param arr
+     */
+    public static void pathsToDitans(int[] arr) {
+        setPast(arr);
+        setCounArr(arr);
+    }
+
+    /**
+     *计算不同距离城市的个数
+     * @param arr
+     */
+    private static void setCounArr(int[] arr) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            res = arr[i];
+            if (res < 0) {
+                arr[i] = 0;
+                while (res < 0) {
+                    int m = -res;
+                    if (arr[m] < 0) {
+                        res = arr[m];
+                        arr[m] = 1;
+                    } else {
+                        arr[m]++;
+                        break;
+                    }
+                }
+            }
+        }
+        arr[0] = 1;
+    }
+
+    /**
+     * 计算没个城市到首都的距离
+     * @param arr
+     */
+    private static void setPast(int[] arr) {
+        int pa = 0;
+        int i = arr[0];
+        arr[0] = -1;
+        int d = 0;
+        while (arr[i] >= 0 && arr[i] != i) {
+            int p = arr[i];
+            arr[i] = d;
+            d = i;
+            i = p;
+        }
+        pa = i;
+        int res = -1;
+        while (arr[d] >= 0) {
+            int s = arr[d];
+            arr[d] = res--;
+            d = s;
+        }
+        arr[d] = res;
+        for (int x = 1; x < arr.length; x++) {
+            if (arr[x] >= 0) {
+                int s = arr[x];
+                if (pa != x) {
+                    arr[x] = -1;
+                }
+                while (arr[s] >= 0 && arr[s] != s) {
+                    int m = arr[s];
+                    arr[s] = x;
+                    x = s;
+                    s = m;
+                }
+                s = arr[s] < 0 ? arr[s] : 0;
+                while (arr[x] >= 0 && arr[x] != x) {
+                    int m = arr[x];
+                    arr[x] = --s;
+                    x = m;
+                }
+                if (pa != x) {
+                    arr[x] = --s;
+                }
+            }
+        }
+        arr[pa] = 0;
+
+    }
+}
